101ab1c7d39d7558a7a11789b2549ef9e2acd7e2
AlexandruD L-Dell <AlexandruD@hotmail.com>
2023-05-05 15:51:28 +0300

    no message this time

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Testing files inside</p>'; ?>
+        <?php echo '<p>Testing files inside and back to SR</p>'; ?>
     </body>
 </html>
