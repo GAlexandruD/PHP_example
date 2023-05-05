455936ceb0a3df9e199d791cc813766943d31934
AlexandruD L-Dell <AlexandruD@hotmail.com>
2023-05-05 17:38:25 +0300

    Wow! Another one!

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Testing with NFS files inside!</p>'; ?>
+        <?php echo '<p>Testing with NFS files inside. Part 2!</p>'; ?>
     </body>
 </html>
