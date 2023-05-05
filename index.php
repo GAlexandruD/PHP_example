1e4d08d6260bb8915f2693879796df8b925cef89
AlexandruD L-Dell <AlexandruD@hotmail.com>
2023-05-05 15:27:31 +0300

    for mr

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Single Release modified today.</p>'; ?>
+        <?php echo '<p>Single Release modified again for mr.</p>'; ?>
     </body>
 </html>
